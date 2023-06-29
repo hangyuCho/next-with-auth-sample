f6df577a9471aa46fb5bf2c03b928f9f74e66b32
hangyuCho <chg3376@gmail.com>
2023-06-30 00:07:55 +0900

    fix : add route and connect db for app

---
+++ lib/prisma.ts
@@ -0,0 +1,11 @@
+// npx prisma studio
+// 上記のコマンドを打つとDB状況が確認できる画面が表示される。
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
